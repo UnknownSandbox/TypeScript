a12131dfd9f0c98a133cfae98243ed5ac66ae1f4
Unknown Hero <i.mineev@afterlogic.com>
2017-07-02 13:40:42 +0300

    Variable Declarations

---
+++ src/base/variableDeclarations.ts
@@ -0,0 +1,52 @@
+//Simple
+var a1 = 10;
+var a1 = 20;
+let hello = "Hello!";
+const x = 42;
+//errors
+//x=41;
+//let hello = "World";
+
+//output: 3 3 3
+// for (var i = 0; i < 3 ; i++) {
+//     setTimeout(function() { console.log(i); }, 100 * i);
+// }
+
+//output: 1 2 3
+// for (let i = 0; i < 3 ; i++) {
+//     setTimeout(function() { console.log(i); }, 100 * i);
+// }
+
+//Destructuring
+let input = [1, 2];
+let [first, second] = input;
+let [first1, ...rest] = [1, 2, 3, 4];
+
+let o = {
+    a: "foo",
+    b: 12,
+    c: "bar"
+};
+var { a , ...passthrough } = o;
+let total = passthrough.b + passthrough.c.length;
+var { a: newName1, b: newName2 } = o; // alias
+
+//Spread
+let firstPart = [1, 2];
+let secondPart = [3, 4];
+let bothPlus = [0, ...firstPart, ...secondPart, 5];
+
+let defaults = { food: "spicy", price: "$$", ambiance: "noisy" };
+let search = { ...defaults, food: "rich" };
+
+class C {
+    p = 12;
+    m() {
+    }
+}
+let c = new C();
+let clone = { ...c };
+//clone.p; // ok, but IDEA dont understand :)
+//clone.m(); // error!
+
+console.log("\x1b[32m There's no actions. Just code \x1b[0m");
