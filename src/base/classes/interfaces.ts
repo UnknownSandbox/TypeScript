48242379e26b5bfbdb18ff93fd0b344583cf94ad
Unknown Hero <unknownpostie@gmail.com>
2017-07-25 11:44:40 +0300

    classes

--- src/base/classes/interfaces.ts
+++ src/base/classes/interfaces.ts
@@ -47,3 +47,22 @@ class Clock implements ClockInterface {
     currentTime: Date;
     constructor(h: number, m: number) { }
 }
+
+//hybrid
+interface Counter {
+    (start: number): string;
+    interval: number;
+    reset(): void;
+}
+
+function getCounter(): Counter {
+    let counter = <Counter>function (start: number) { };
+    counter.interval = 123;
+    counter.reset = function () { };
+    return counter;
+}
+
+let c = getCounter();
+c(10);
+c.reset();
+c.interval = 5.0;
